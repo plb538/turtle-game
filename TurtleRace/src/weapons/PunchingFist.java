7530f191fb16cf2dfb4f42dc0cc39a24f32bea8f
steve <sts738@mun.ca>
2016-02-22 13:20:55 -0330

    Made all weapon stats static final

--- TurtleRace/src/weapons/PunchingFist.java
+++ TurtleRace/src/weapons/PunchingFist.java
@@ -3,18 +3,18 @@ package weapons;
 public class PunchingFist implements Weapon{
 
 	//Damage per hit
-	int damage = 34;
+	static final int damage = 34;
 	
 	//Range to hit another character
-	int range = common.Character.width;
+	static final int range = common.Character.width;
 	
 	//Seconds between attacks
-	int attackspeed = 2;
+	static final int attackspeed = 2;
 	
 	//Vertical attack height for hit detection
-	int attackheight = (int)(0.5*common.Character.height);
+	static final int attackheight = (int)(0.5*common.Character.height);
 	
 	//Weapon name
-	String name = "Fist";
+	static final String name = "Fist";
 	
 }
