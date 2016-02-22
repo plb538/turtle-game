f1a4f82ebbfa7a9b21c709e4fdf87af0bcaa4a19
Patrick Bonnell <plb538@live.ca>
2016-02-22 21:54:13 +0000

    c

---
+++ TurtleRace/src/TileMap/Background.java
@@ -0,0 +1,51 @@
+package TileMap;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import Main.GamePanel;
+
+public class Background{
+	
+	private BufferedImage image;
+	
+	private double x, y, dx, dy;
+	private double moveScale;
+	
+	public Background(String path, double ms){
+		try{
+			image = ImageIO.read(getClass().getResourceAsStream(path));
+			moveScale = ms;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void setPosition(double x, double y){
+		this.x = (x * moveScale) % GamePanel.WIDTH;
+		this.y = (y * moveScale) % GamePanel.HEIGHT;
+	}
+	
+	public void setVector(double dx, double dy){
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public void update(){
+		x += dx;
+		y += dy;
+	}
+	
+	public void draw(Graphics2D g){
+		g.drawImage(image, (int)x, (int)y, null);
+		if(x < 0){
+			g.drawImage(image, (int)x + GamePanel.WIDTH, (int)y, null);
+		}
+		if(x > 0){
+			g.drawImage(image, (int)x - GamePanel.WIDTH, (int)y, null);
+		}
+	}
+}
