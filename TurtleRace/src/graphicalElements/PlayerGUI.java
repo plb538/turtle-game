82ac0328ae301d87a366b7603184171ab7eec87b
steve <sts738@mun.ca>
2016-02-22 16:04:30 -0330

    Creating fields in PlayerGUI

--- TurtleRace/src/graphicalElements/PlayerGUI.java
+++ TurtleRace/src/graphicalElements/PlayerGUI.java
@@ -2,4 +2,13 @@ package graphicalElements;
 
 public class PlayerGUI extends GraphicalElement{
 
+	private DrawnHealth myHealth = null;
+	private DrawnProgress myProgress = null;
+	private DrawnWeapon myWeapon = null;
+	private playerElements.Player myPlayer = null;
+	
+	public PlayerGUI(playerElements.Player _myPlayer){
+		myPlayer = _myPlayer;		
+	}
+	
 }
