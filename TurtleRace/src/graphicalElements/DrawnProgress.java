1c93437670b1cc46cda7cd05afbf5a02b1ff7e20
plb53 <plb53@DESKTOP-CC7TIR7>
2016-02-24 13:21:01 -0330

    Random crap

--- TurtleRace/src/graphicalElements/DrawnProgress.java
+++ TurtleRace/src/graphicalElements/DrawnProgress.java
@@ -2,7 +2,9 @@ package graphicalElements;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
@@ -12,11 +14,18 @@ public class DrawnProgress extends GraphicalElement{
 	private JPanel progBar;
 	private JProgressBar jbar;
 	private Player p1;
+	private BufferedImage image;
 	
 	public DrawnProgress(){
 		jbar = new JProgressBar(0, 100);
 		jbar.setStringPainted(true);
 		jbar.setVisible(true);
+		try{
+			image = ImageIO.read(getClass().getResourceAsStream("/sprites/temp-turtle.PNG"));
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	public void update(){
