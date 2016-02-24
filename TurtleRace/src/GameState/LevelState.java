8eb0a64a540fa9ed24fc456856bdd17656b8bea6
plb53 <plb53@DESKTOP-CC7TIR7>
2016-02-24 11:48:10 -0330

    Dragon moves

--- TurtleRace/src/GameState/LevelState.java
+++ TurtleRace/src/GameState/LevelState.java
@@ -1,17 +1,17 @@
 package GameState;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
-import Main.GamePanel;
+import Entity.Player;
 import TileMap.Background;
 import TileMap.TileMap;
 
 public class LevelState extends GameState{
 
-	//private TileMap tileMap;
+	private TileMap tileMap;
 	private Background bg;
+	private Player player;
 	
 	public LevelState(GameStateManager gsm){
 		this.gsm = gsm;
@@ -20,17 +20,21 @@ public class LevelState extends GameState{
 	
 	@Override
 	public void init(){
-		//tileMap = new TileMap(30); //value passed to constructor will depend on the tile map
-		//tileMap.loadTiles("/TileSets/grasstileset.gif");
-		//tileMap.loadMap("/Maps/level1-1.map");
-		//tileMap.setPosition(0, 0);
+		tileMap = new TileMap(30); //value passed to constructor will depend on the tile map
+		tileMap.loadTiles("/TileSets/grasstileset.gif");
+		tileMap.loadMap("/Maps/level1-1.map");
+		tileMap.setPosition(0, 0);
 		
 		bg = new Background("/Backgrounds/default-background.png", 1);	
+		player = new Player(tileMap);
+		player.setPosition(100, 100);
 		
 	}
 
 	@Override
-	public void update(){		
+	public void update(){	
+		player.update();
+	
 	}
 
 	@Override
@@ -40,6 +44,8 @@ public class LevelState extends GameState{
 		
 		//draw tile map
 		//tileMap.draw(g);
+		//draw player
+		player.draw(g);
 		
 	}
 
@@ -48,9 +54,24 @@ public class LevelState extends GameState{
 		if(k == KeyEvent.VK_ENTER){
 			gsm.setState(4);
 		}
+		if(k == KeyEvent.VK_LEFT) player.setLeft(true);
+		if(k == KeyEvent.VK_RIGHT) player.setRight(true);
+		if(k == KeyEvent.VK_UP) player.setUp(true);
+		if(k == KeyEvent.VK_DOWN) player.setDown(true);
+		if(k == KeyEvent.VK_W) player.setJumping(true);
+		if(k == KeyEvent.VK_Q) player.setGliding(true);
+		if(k == KeyEvent.VK_E) player.setScratching();
+		if(k == KeyEvent.VK_R) player.setFiring();
 	}
 
 	@Override
-	public void keyReleased(int k){}
+	public void keyReleased(int k){
+		if(k == KeyEvent.VK_LEFT) player.setLeft(false);
+		if(k == KeyEvent.VK_RIGHT) player.setRight(false);
+		if(k == KeyEvent.VK_UP) player.setUp(false);
+		if(k == KeyEvent.VK_DOWN) player.setDown(false);
+		if(k == KeyEvent.VK_W) player.setJumping(false);
+		if(k == KeyEvent.VK_Q) player.setGliding(false);
+	}
 	
 }
