dc59d8e572106c86b96cd3a61c75eefb2f8004c6
Patrick Bonnell <plb538@live.ca>
2016-03-11 23:12:21 +0000

    commented

--- TurtleRace/src/TileMap/Tile.java
+++ TurtleRace/src/TileMap/Tile.java
@@ -4,13 +4,13 @@ import java.awt.image.BufferedImage;
 
 public class Tile{
 
-	private BufferedImage image;
-	private int type;
+	private BufferedImage image; //tile sprite
+	private int type; 
 	
-	//types
-	public static final int NORMAL = 0;
-	public static final int BLOCKED = 1;
+	public static final int NORMAL = 0; //player moves through
+	public static final int BLOCKED = 1; //player collides with
 	
+	//each tile gets an Image object and is set a type
 	public Tile(BufferedImage image, int type){
 		this.image = image;
 		this.type = type;
