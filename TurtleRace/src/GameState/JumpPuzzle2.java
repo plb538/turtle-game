0c6331de295888c719079ef4030130a5f303b0b6
steve <steve@DESKTOP-17B01SO>
2016-04-03 18:53:27 -0230

    New maps

---
+++ TurtleRace/src/GameState/JumpPuzzle2.java
@@ -0,0 +1,66 @@
+package GameState;
+
+import java.util.ArrayList;
+
+import Entity.MonkeyEnemy;
+import Entity.Portal;
+import Main.Game;
+import Main.GamePanel;
+import TileMap.Background;
+import TileMap.TileMap;
+import graphicalElements.DrawnHealth;
+import graphicalElements.DrawnProgress;
+
+//basically the same as LevelState but with different map
+public class JumpPuzzle2 extends GameState{
+	
+	private MonkeyEnemy m1;
+	private MonkeyEnemy m2;
+	private MonkeyEnemy m3;
+	private MonkeyEnemy m4;
+	
+	public JumpPuzzle2(GameStateManager gsm){
+		this.gsm = gsm;
+		init();
+	}
+	
+	@Override
+	public void init(){
+		tileMap = new TileMap(64);
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("CrapSky");
+		list.add("CrapBlock");
+		list.add("CrapDirtBlock");
+		
+		tileMap.loadTiles("/TileSets/Terrain", list);
+		tileMap.loadMap("/Maps/JumpPuzzle2.map");
+		tileMap.setPosition(0, 0);
+		
+		bg = new Background("/Backgrounds/default-background.png", 1);	
+		
+		Game.p1.setObjectOnTileMap(tileMap);
+		Game.p1.setPosition(100, tileMap.getHeight() - 200);
+		
+		monkeys = new ArrayList<MonkeyEnemy>();
+		m1 = new MonkeyEnemy(tileMap);
+		m2 = new MonkeyEnemy(tileMap);
+		m3 = new MonkeyEnemy(tileMap);
+		m4 = new MonkeyEnemy(tileMap);
+		monkeys.add(m1);
+		monkeys.add(m2);
+		monkeys.add(m3);
+		monkeys.add(m4);
+		m1.setPosition(200, 600);
+		m2.setPosition(1500, 350);
+		m3.setPosition(700, 500);
+		m4.setPosition(900, 500);
+		
+		portal = new Portal(tileMap);
+		portal.setPosition(tileMap.getWidth() - 1.5*tileMap.getTileSize(), 100);
+		
+		healthBar = new DrawnHealth(Game.p1);
+		progress = new DrawnProgress(Game.p1, tileMap);
+	}
+}
---
+++ TurtleRace/src/GameState/JumpingLevel1.java
@@ -0,0 +1,77 @@
+package GameState;
+
+import java.util.ArrayList;
+
+import Entity.MonkeyEnemy;
+import Entity.Player;
+import Entity.Portal;
+import Main.Game;
+import TileMap.Background;
+import TileMap.TileMap;
+import graphicalElements.DrawnHealth;
+import graphicalElements.DrawnProgress;
+import graphicalElements.PlayerTip;
+import networking.InformationPacket;
+
+public class JumpingLevel1 extends GameState{
+		
+	private MonkeyEnemy m1;
+	private MonkeyEnemy m2;
+	
+	public JumpingLevel1(GameStateManager gsm){
+		this.gsm = gsm;
+		init();
+	}
+	
+	@Override
+	public void init(){
+		
+		tileMap = new TileMap(64);
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		//adds tiles from resource folder
+		list.add("CrapSky");
+		list.add("CrapBlock");
+		list.add("CrapDirtBlock");
+		tileMap.loadTiles("/TileSets/Terrain", list);
+		
+		//load map from resource folder
+		tileMap.loadMap("/Maps/JumpingLevel1.map");
+		
+		tileMap.setPosition(0, 0);
+		
+		//load background from resource folder
+		bg = new Background("/Backgrounds/default-background.png", 1);	
+		
+		//creates player and sets player's tilemap to LevelState's tilemap
+		Game.p1.setObjectOnTileMap(tileMap);
+		Game.p1.setPosition(100, tileMap.getHeight() - 100);
+		
+		monkeys = new ArrayList<MonkeyEnemy>();
+		
+		m1 = new MonkeyEnemy(tileMap);
+		m2 = new MonkeyEnemy(tileMap);
+		monkeys.add(m1);
+		monkeys.add(m2);
+		m1.setPosition(600, 450);
+		m2.setPosition(1200, 150);
+		
+        //creates portal 
+		portal = new Portal(tileMap);
+		portal.setPosition(tileMap.getWidth()-96*2, 450);
+		
+		//creates player one's health bar and progress bar
+		healthBar = new DrawnHealth(Game.p1);
+		progress = new DrawnProgress(Game.p1, tileMap);
+		
+		//Create player 2 objects
+		Game.p2.setObjectOnTileMap(tileMap);
+		Game.p2.setPosition(100, tileMap.getHeight()-100);
+			
+		healthBar2 = new DrawnHealth(Game.p2);
+		progress2 = new DrawnProgress(Game.p2, tileMap);
+		pt = new PlayerTip("Press Up to jump and W to attack", 180, 250);
+	}
+}
+
